407b7dd7dfafc1f858a1434ab628b8721fe1c7b5
nlusn <abobafettq@gmail.com>
2026-02-09 13:20:04 +0600

    Fix formatting issues in RewardToken.sol

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RewardToken is ERC20, Ownable {
-
     constructor() ERC20("RewardToken", "RWD") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
